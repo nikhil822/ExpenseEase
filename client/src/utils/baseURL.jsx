c848b8b71eead703d2c1102a99323dec77e9fc6f
Nikhil Kumar Sahu <75385653+nikhil822@users.noreply.github.com>
2024-04-07 10:28:47 +0530

    Update baseURL.jsx

--- client/src/utils/baseURL.jsx
+++ client/src/utils/baseURL.jsx
@@ -1,4 +1,4 @@
 // const baseURL = 'http://localhost:5000/api'
-const baseURL = 'https://expense-ease-backend.vercel.app/api'
+const baseURL = 'https://expense-ease-backend.vercel.app'
 
 export default baseURL
